18d5e6bd3f9aa676dc96f59caf49cc9a922bdef5
Eniola Ajiboye <eloyjaws@gmail.com>
2021-11-22 22:05:25 +0200

    Test flourish with streamlit

---
+++ app/app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+
+components.html('<div class="flourish-embed flourish-chart" data-src="visualisation/7903916"><script src="https://public.flourish.studio/resources/embed.js"></script></div>', height=600)
