01581ad4dec4139b688595e9cfd6b6539b09eb89
Eniola Ajiboye <eloyjaws@gmail.com>
2021-12-05 08:02:33 +0200

    Add logo and title

--- app/app.py
+++ app/app.py
@@ -1,5 +1,30 @@
+import sys
+sys.path.append("..")
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import altair as alt
+from PIL import Image
+
 import streamlit as st
 import streamlit.components.v1 as components
 
+import utils
+import plots
+import text
 
-components.html('<div class="flourish-embed flourish-chart" data-src="visualisation/7903916"><script src="https://public.flourish.studio/resources/embed.js"></script></div>', height=600)
+
+logo = Image.open("world.png")
+st.image(
+    logo,
+    width=100,
+)
+
+st.title(text.Title)
+# world_bank_screenshot = Image.open("world_bank.png")
+
+
+
+# components.html('<div class="flourish-embed flourish-chart" data-src="visualisation/7903916"><script src="https://public.flourish.studio/resources/embed.js"></script></div>', height=600)
---
+++ app/text.py
@@ -0,0 +1,48 @@
+Title = "The role of the internet in social connections [Boilerplate title]"
+
+Paragraph_1 = ""
+Paragraph_2 = ""
+Paragraph_3 = ""
+
+Questions = """
+                1. What are the Top K ?
+                2. What are the ?
+                3. Is there a connection beteween x and y?
+                4. Which Countries ?
+                5. Does ?
+                6. Is ?
+            """
+
+Conclusion = """
+                Insert Conclusion Here
+            """
+
+References = """
+                - COVID-19 Logo obtained from https://www.un.org/en/coronavirus
+                
+                - 'Absolutely unacceptable' vaccination rates in developing countries. World Bank. (2021, August 3). 
+                    Retrieved October 19, 2021, 
+                    from https://www.worldbank.org/en/news/podcast/2021/07/30/-absolutely-unacceptable-vaccination-rates-in-developing-countries-the-development-podcast. 
+
+                - Disparities in global vaccination progress are large and growing, with low-income countries  and those in Africa lagging behind. KFF. Anderson, E. (2021, July 21). 
+                    Retrieved October 20, 2021, 
+                    from https://www.kff.org/coronavirus-covid-19/press-release/disparities-in-global-vaccination-progress-are-large-and-growing-with-low-income-countries-and-those-in-africa-lagging-behind/. 
+            """
+
+Footer = """ <style>
+    .footer {
+    position: fixed;
+    left: 16;
+    bottom: 0;
+    }
+    </style>
+    <div class="footer">
+    <p>An Interactive Web App by 
+        <ul>
+            <li>Eniola Ajiboye (eajiboye)</li>
+            <li>Guo [Timur] Chen (gchen2)</li>
+            <li>Rukayat Sadiq (rsadiq)</li>
+        </ul>
+        &nbsp;
+    </div>
+    """
